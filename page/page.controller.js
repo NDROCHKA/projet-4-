3f54a1989619b1a05d3bb332c006b70d47097941
JohnBZ24 <johnj0000@icloud.com>
2025-11-23 19:15:34 +0200

    goofty ahhh

--- page/page.controller.js
+++ page/page.controller.js
@@ -4,11 +4,8 @@ import pageService from "./page.service.js";
 class pageController {
   static async createPage(req, res) {
     try {
-      const { email, userId } = req.body;
-      if (!email || !userId) {
-        return res.status(400).json({ message: "email and userId are required" });
-      }
-      await pageService.createPage(email, userId);
+      const { userId, email } = req.body; // Get both from body
+      await pageService.createPage(userId, email);
       res.status(201).json({
         statusCode: 201,
         message: "page created",
